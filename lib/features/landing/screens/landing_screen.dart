b187781cf865394620efdc1757b0f5ceb284e96d
shahbajjamil <shahbajjamil2014@gmail.com>
2022-08-03 23:23:10 +0530

    fireb setup, landing page

---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
+import 'package:whatsapp_ui/common/widgets/common_button.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+            const Text(
+              "Welcome to WhatsApp",
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: size.height * 0.1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Image.asset(
+                "assets/bg.png",
+                width: size.width,
+                height: size.width,
+                color: tabColor,
+              ),
+            ),
+            SizedBox(height: size.height * 0.1),
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
+                style: TextStyle(color: greyColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: size.width * 0.75,
+              child: CustomButton(
+                text: 'AGREE AND CONTINUE',
+                onPressed: () {
+                  // Navigator.pushNamed(context, '/mobile');
+                },
+                // onPressed: () => navigateToLoginScreen(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
